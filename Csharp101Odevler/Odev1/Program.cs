2fb712847e082501a9ee6f53a89fe6e1d30c1386
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-05-22 15:33:31 +0300

    Csharp 101 Ödev 1

--- Csharp101Odevler/Odev1/Program.cs
+++ Csharp101Odevler/Odev1/Program.cs
@@ -8,122 +8,122 @@
             //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
             // Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
             // Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
-            //Console.WriteLine("***** Soru 1 *****");
-            //Console.Write("Girmek istediğiniz sayı adedini giriniz: ");
-            //int n = Convert.ToInt32(Console.ReadLine());
-            //if (n < 0)
-            //{
-            //    Console.WriteLine("Lütfen pozitif bir sayı giriniz!");
-            //}
-            //else
-            //{
-            //    int[] numbers = new int[n];
-            //    for (int i = 0; i < n; i++)
-            //    {
-            //        Console.Write($"{i + 1}. sayıyı giriniz: ");
-            //        int sayi = Convert.ToInt32(Console.ReadLine());
-            //        if (sayi > 0)
-            //        {
-            //            numbers[i] = sayi;
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("Lütfen pozitif bir sayı giriniz.");
-            //            i--;
-            //        }              
-            //    }
-            //    Console.WriteLine("Çift Sayılar:");
-            //    for (int i = 0; i < n; i++)
-            //    {
-            //        if (numbers[i] % 2 == 0)
-            //        {
-            //            Console.WriteLine(numbers[i]);
-            //        }
-            //    }
+            Console.WriteLine("***** Soru 1 *****");
+            Console.Write("Girmek istediğiniz sayı adedini giriniz: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0)
+            {
+               Console.WriteLine("Lütfen pozitif bir sayı giriniz!");
+            }
+            else
+            {
+               int[] numbers = new int[n];
+               for (int i = 0; i < n; i++)
+               {
+                   Console.Write($"{i + 1}. sayıyı giriniz: ");
+                   int sayi = Convert.ToInt32(Console.ReadLine());
+                   if (sayi > 0)
+                   {
+                       numbers[i] = sayi;
+                   }
+                   else
+                   {
+                       Console.WriteLine("Lütfen pozitif bir sayı giriniz.");
+                       i--;
+                   }              
+               }
+               Console.WriteLine("Çift Sayılar:");
+               for (int i = 0; i < n; i++)
+               {
+                   if (numbers[i] % 2 == 0)
+                   {
+                       Console.WriteLine(numbers[i]);
+                   }
+               }
 
-            //}
+            }
             //------------------------------------------------------------------//
             //2.soru
             // Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). 
             // Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
             // Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
-            //Console.WriteLine("***** Soru 2 *****");
-            //int n;
-            //do
-            //{
-            //    Console.Write("Girmek istediğiniz sayı adedini giriniz: ");
-            //    n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***** Soru 2 *****");
+            int n;
+            do
+            {
+               Console.Write("Girmek istediğiniz sayı adedini giriniz: ");
+               n = Convert.ToInt32(Console.ReadLine());
 
-            //    if (n <= 0)
-            //    {
-            //        Console.WriteLine("Lütfen pozitif bir sayı giriniz!");
-            //    }
-            //}
-            //while (n <= 0);
-            //int m;
-            //do
-            //{
-            //    Console.Write("Bölünecek sayıyı giriniz: ");
-            //    m = Convert.ToInt32(Console.ReadLine());
+               if (n <= 0)
+               {
+                   Console.WriteLine("Lütfen pozitif bir sayı giriniz!");
+               }
+            }
+            while (n <= 0);
+            int m;
+            do
+            {
+               Console.Write("Bölünecek sayıyı giriniz: ");
+               m = Convert.ToInt32(Console.ReadLine());
 
-            //    if (m <= 0)
-            //    {
-            //        Console.WriteLine("Lütfen pozitif bir sayı giriniz!");
-            //    }
-            //}
-            //while (m <= 0);
-            //int[] numbers = new int[n];
-            //for (int i = 0; i < n; i++)
-            //{
-            //    Console.Write($"{i + 1}. sayıyı giriniz: ");
-            //    int sayi = Convert.ToInt32(Console.ReadLine());
-            //    while (sayi <= 0) 
-            //    {
-            //        Console.WriteLine("Lütfen pozitif bir sayı giriniz.");
-            //        Console.Write($"{i + 1}. sayıyı giriniz: ");
-            //        sayi = Convert.ToInt32(Console.ReadLine());
-            //    }
-            //    numbers[i] = sayi;
-            //}
+               if (m <= 0)
+               {
+                   Console.WriteLine("Lütfen pozitif bir sayı giriniz!");
+               }
+            }
+            while (m <= 0);
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+               Console.Write($"{i + 1}. sayıyı giriniz: ");
+               int sayi = Convert.ToInt32(Console.ReadLine());
+               while (sayi <= 0) 
+               {
+                   Console.WriteLine("Lütfen pozitif bir sayı giriniz.");
+                   Console.Write($"{i + 1}. sayıyı giriniz: ");
+                   sayi = Convert.ToInt32(Console.ReadLine());
+               }
+               numbers[i] = sayi;
+            }
 
-            //Console.WriteLine($"{m} sayısına tam bölünen ya da eşit olan sayılar:");
-            //for (int i = 0; i < n; i++)
-            //{
-            //    if (numbers[i] % m == 0 || numbers[i] == m)
-            //    {
-            //        Console.WriteLine($"Tam Bölünen veya eşit Sayılar: {numbers[i]}");
-            //    }
-            //}
+            Console.WriteLine($"{m} sayısına tam bölünen ya da eşit olan sayılar:");
+            for (int i = 0; i < n; i++)
+            {
+               if (numbers[i] % m == 0 || numbers[i] == m)
+               {
+                   Console.WriteLine($"Tam Bölünen veya eşit Sayılar: {numbers[i]}");
+               }
+            }
             //------------------------------------------------------------------//
             //3.soru
             // Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n).
             // Sonrasında kullanıcıdan n adet kelime girmesi isteyin.
             // Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
-            //Console.WriteLine("***** Soru 3 *****");
-            //int n;
-            //do
-            //{
-            //    Console.Write("Girmek istediğiniz kelime adedini giriniz: ");
-            //    n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***** Soru 3 *****");
+            int n;
+            do
+            {
+               Console.Write("Girmek istediğiniz kelime adedini giriniz: ");
+               n = Convert.ToInt32(Console.ReadLine());
 
-            //    if (n <= 0)
-            //    {
-            //        Console.WriteLine("Lütfen pozitif bir sayı giriniz!");
-            //    }
-            //}
-            //while (n <= 0);
-            //string[] words = new string[n];
-            //for (int i = 0; i < n; i++)
-            //{
-            //    Console.Write($"{i + 1}. kelimeyi giriniz: ");
-            //    string kelime = Console.ReadLine();
-            //    words[i] = kelime;
-            //}
-            //Console.WriteLine("Girilen kelimelerin tersten yazılışı:");
-            //for (int i = n - 1; i >= 0; i--)
-            //{
-            //    Console.WriteLine(words[i]);
-            //}
+               if (n <= 0)
+               {
+                   Console.WriteLine("Lütfen pozitif bir sayı giriniz!");
+               }
+            }
+            while (n <= 0);
+            string[] words = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+               Console.Write($"{i + 1}. kelimeyi giriniz: ");
+               string kelime = Console.ReadLine();
+               words[i] = kelime;
+            }
+            Console.WriteLine("Girilen kelimelerin tersten yazılışı:");
+            for (int i = n - 1; i >= 0; i--)
+            {
+               Console.WriteLine(words[i]);
+            }
             //------------------------------------------------------------------//
             //4.soru
             //Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin.
